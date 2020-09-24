21b83f86c86e04e866d7beb758dd78ff8d783a6c
舒琪航 <36402@PC-03650>
2020-09-24 15:27:31 +0800

    C++

---
+++ LeetCode.C++/LeetCode.C++.cpp
@@ -0,0 +1,84 @@
+﻿// LeetCode.C++.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+//祖先类
+class R
+{
+private:
+	int r;
+public:
+	R(int x = 0) :r(x) {};
+	void f()
+	{
+		cout << "r" << r << endl;
+	}
+	void print()
+	{
+		cout << "print R:" << r << endl;
+	}
+};
+
+//父类A
+class A :public R
+{
+private:
+	int a;
+public:
+	A(int x) :a(x) {};
+	void f()
+	{
+		cout << "a=" << a << endl;
+		//R::f();//调用的父类的f函数
+	}
+};
+
+//父类B
+class B :public R
+{
+private:
+	int b;
+public:
+	B(int x) :b(x) {};
+	void f()
+	{
+		cout << "b=" << b << endl;
+		//R::f();//调用的父类的f函数
+	}
+};
+
+//子类C
+class C :public A, public B
+{
+private:
+	int c;
+public:
+	C(int x = 1) :c(x), A(x), B(x) {};
+	void f()
+	{
+		cout << "c" << c << endl;
+		//A::f();
+		//B::f();
+	}
+};
+
+
+int main()
+{
+	C cc(1);
+	cc.f();
+	cout << "Hello World!\n";
+	return 0;
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ LeetCode/Tree/501/501Solution.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace LeetCode.Tree._501
+{
+    class _501Solution
+    {
+
+        //给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+
+        //假定 BST 有如下定义：
+
+        //结点左子树中所含结点的值小于等于当前结点的值
+        //结点右子树中所含结点的值大于等于当前结点的值
+        //左子树和右子树都是二叉搜索树
+        //例如：
+        //给定 BST[1, null, 2, 2],
+
+        //   1
+        //    \
+        //     2
+        //    /
+        //   2
+        //返回[2].
+
+        //提示：如果众数超过1个，不需考虑输出顺序
+
+        //进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+        TreeNode pnode = null;//父节点
+        int count, max;
+        public int[] FindMode(TreeNode root)
+        {
+            IList<int> result = new List<int>();
+            MediumSort(root, result);
+            return result.ToArray();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node">当前节点</param>
+        /// <param name="result"></param>
+        /// <param name="count">当前节点之前连续的</param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        void MediumSort(TreeNode node, IList<int> result)
+        {
+            if (node == null)
+            {
+                return ;
+            }
+            MediumSort(node.left, result);
+            if (node.val == pnode?.val)
+            {
+                count = count + 1;
+            }
+            else
+            {
+                count = 1;
+            }
+            pnode = node;
+            if (count > max)
+            {
+                max = count;
+                result.Clear();
+            }
+            if (count >= max)
+            {
+                result.Add(node.val);
+            }
+            Console.WriteLine($"node:{node.val},pnode:{pnode?.val},max:{max},count:{count},keys:{string.Join(',', result)}");
+            MediumSort(node.right, result);
+        }
+    }
+}
