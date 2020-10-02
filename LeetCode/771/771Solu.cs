270d0e2ea4e25266324bca9bc193a0a59797de75
Evil <sqh1449265660@outlook.com>
2020-10-02 11:35:49 +0800

    dp

---
+++ LeetCode/771/771Solu.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._771
+{
+    class _771Solu
+    {
+        //给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+        //J 中的字母不重复，J 和S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+        //示例 1:
+
+        //输入: J = "aA", S = "aAAbbbb"
+        //输出: 3
+        //示例 2:
+
+        //输入: J = "z", S = "ZZ"
+        //输出: 0
+
+        //来源：力扣（LeetCode）
+        //链接：https://leetcode-cn.com/problems/jewels-and-stones
+        public int NumJewelsInStones(string J, string S)
+        {
+            int sum = 0;
+            var sa = S.ToCharArray();
+            var ja = J.ToCharArray();
+            Dictionary<char, int> dc = new Dictionary<char, int>();
+            for (int i = 0; i < sa.Length; i++)
+            {
+                if (dc.ContainsKey(sa[i]))
+                {
+                    dc[sa[i]]++;
+                }
+                else
+                {
+                    dc.Add(sa[i], 1);
+                }
+            }
+            for (int i = 0; i < ja.Length; i++)
+            {
+                if (dc.ContainsKey(ja[i]))
+                {
+                    sum += dc[ja[i]];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/DynamicPrograming/19/19Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.DynamicPrograming._19
+{
+    class _19Solution
+    {
+        //小扣出去秋游，途中收集了一些红叶和黄叶，他利用这些叶子初步整理了一份秋叶收藏集 leaves， 字符串 leaves 仅包含小写字符 r 和 y， 其中字符 r 表示一片红叶，字符 y 表示一片黄叶。
+        //出于美观整齐的考虑，小扣想要将收藏集中树叶的排列调整成「红、黄、红」三部分。每部分树叶数量可以不相等，但均需大于等于 1。每次调整操作，小扣可以将一片红叶替换成黄叶或者将一片黄叶替换成红叶。请问小扣最少需要多少次调整操作才能将秋叶收藏集调整完毕。
+
+        //示例 1：
+
+        //输入：leaves = "rrryyyrryyyrr"
+
+        //输出：2
+
+        //解释：调整两次，将中间的两片红叶替换成黄叶，得到 "rrryyyyyyyyrr"
+
+        //示例 2：
+
+        //输入：leaves = "ryr"
+
+        //输出：0
+
+        //解释：已符合要求，不需要额外操作
+
+        //提示：
+
+        //3 <= leaves.length <= 10^5
+        //leaves 中只包含字符 'r' 和字符 'y'
+
+        //来源：力扣（LeetCode）
+        //链接：https://leetcode-cn.com/problems/UlBDOe
+
+        public int MinimumOperations(string leaves)
+        {
+            int[][] dp = new int[leaves.Length][];
+            var a = leaves.ToCharArray();
+            dp[0] = new int[3];
+            dp[0][0] = isYellow(a[0]);
+            dp[0][1] = int.MaxValue;
+            dp[0][2] = int.MaxValue;
+            for (int i = 1; i < leaves.Length; i++)
+            {
+                dp[i] = new int[3];
+                dp[i][0] = dp[i - 1][0] + isYellow(a[i]);
+                dp[i][1] = Math.Min(dp[i - 1][0], dp[i - 1][1]) + isRed(a[i]);
+                if (i > 1)
+                {
+                    dp[i][2] = Math.Min(dp[i - 1][1], dp[i - 1][2]) + isYellow(a[i]);
+                }
+                else
+                {
+                    dp[i][2] = int.MaxValue;//节点为2个时，不满足红黄红，故设置值为极大值
+                }
+
+            }
+            return dp[leaves.Length - 1][2];
+        }
+
+        int isRed(char v)
+        {
+            return 'r' == v ? 1 : 0;
+        }
+        int isYellow(char v)
+        {
+            return 'y' == v ? 1 : 0;
+        }
+    }
+}
