79704e0f46090e18e06daa3f6f83b84a2748369c
舒琪航 <36402@PC-03650>
2020-12-09 17:09:52 +0800

    Sort Algrothm

--- LeetCode/SortAlgrothm/SortTest.cs
+++ LeetCode/SortAlgrothm/SortTest.cs
@@ -19,21 +19,24 @@ namespace LeetCode.SortAlgrothm
         public void Test()
         {
             Console.WriteLine("排序算法：");
-            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
+            
 
             Console.WriteLine("冒泡排序");
+            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
             maopaoSort maopao = new maopaoSort();
             maopao.Sort(nums1);
             Console.WriteLine(string.Join(',', nums1));
             Console.WriteLine("---------------");
 
             Console.WriteLine("插入排序");
+            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
             charuSort charu = new charuSort();
             charu.Sort(nums2);
             Console.WriteLine(string.Join(',', nums2));
             Console.WriteLine("---------------");
 
             Console.WriteLine("快速排序");
+            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
             kuaisuSort kuaisu = new kuaisuSort();
             kuaisu.Sort(nums3);
             Console.WriteLine(string.Join(',', nums3));
@@ -41,9 +44,17 @@ namespace LeetCode.SortAlgrothm
 
 
             Console.WriteLine("选择排序");
+            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
             xuanzeSort xuanze = new xuanzeSort();
             xuanze.Sort(nums4);
             Console.WriteLine(string.Join(',', nums4));
+            Console.WriteLine("---------------");
+
+            Console.WriteLine("归并排序");
+            Console.WriteLine($"原始数据：{string.Join(',', nums)}");
+            guibingSort guibing = new guibingSort();
+            guibing.Sort(nums5);
+            Console.WriteLine(string.Join(',', nums5));
             Console.WriteLine("---------------");
 
             Console.WriteLine("排序算法结束！按回车键结束");
---
+++ LeetCode/SortAlgrothm/guibingSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SortAlgrothm
+{
+    class guibingSort
+    {
+        public void Sort(int[] nums)
+        {
+            Recurvision(nums, 0, nums.Length - 1);
+        }
+        int l = 1;
+        void Recurvision(int[] nums, int s, int e)
+        {
+            if (e - s < 1)
+            {
+                return;
+            }
+            Recurvision(nums, s, (e + s) / 2);
+            Recurvision(nums, (e + s) / 2 + 1, e);
+            Merge(nums, s, e);
+            Console.WriteLine($"第{l++}轮，排序起始点{s}-{e}：{string.Join(',', nums)}");
+        }
+
+        void Merge(int[] nums, int s, int e)
+        {
+            int mid = (s + e) / 2;
+            for (int j = mid + 1; j <= e; j++)//合并不开辟新空间,插入排序，亦可开辟新空间。
+            {
+                if (nums[j] > nums[j - 1])
+                {
+                    return;
+                }
+                int tem = nums[j];
+                int i = j - 1;
+                while (i >= s && nums[i] > tem)
+                {
+                    nums[i + 1] = nums[i];
+                    i--;
+                }
+                nums[i + 1] = tem;
+            }
+        }
+    }
+}
