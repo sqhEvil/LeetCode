5b4b595da90c461dac7cc62daba629a9e6ce799c
舒琪航 <36402@PC-03650>
2021-02-08 10:43:22 +0800

    feat add

---
+++ LeetCode/1423/1423Solution.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._1423
+{
+    class _1423Solution
+    {
+        //几张卡牌 排成一行，每张卡牌都有一个对应的点数。点数由整数数组 cardPoints 给出。
+
+        //每次行动，你可以从行的开头或者末尾拿一张卡牌，最终你必须正好拿 k 张卡牌。
+
+        //你的点数就是你拿到手中的所有卡牌的点数之和。
+
+        //给你一个整数数组 cardPoints 和整数 k，请你返回可以获得的最大点数。
+
+        /// 1 <= cardPoints.length <= 10^5
+        ///1 <= cardPoints[i] <= 10^4
+        //1 <= k <= cardPoints.length
+        /// <summary>
+        /// 初始想法改进，初始想法内存消耗大，直接滑动窗口减少内存
+        /// </summary>
+        /// <param name="cardPoints"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int MaxScore(int[] cardPoints, int k)
+        {
+            int temSum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                temSum += cardPoints[i];
+            }
+            var result = temSum;
+            for (int i = 1; i <= k; i++)
+            {
+                temSum -= cardPoints[k - i];
+                temSum += cardPoints[cardPoints.Length - i];
+                result = Math.Max(result, temSum);
+            }
+            return result;
+        }
+        /// <summary>
+        /// 初始想法，记录2边的和
+        /// </summary>
+        /// <param name="cardPoints"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int MaxScore1(int[] cardPoints, int k)
+        {
+            int result = 0;
+            int[] leftCount = new int[k + 1];
+            int[] rightCount = new int[k + 1];
+            for (int i = 0; i < k; i++)
+            {
+                leftCount[i + 1] = cardPoints[i] + leftCount[i];
+                rightCount[i + 1] = cardPoints[cardPoints.Length - 1 - i] + rightCount[i];
+            }
+            for (int i = 0; i <= k; i++)
+            {
+                result = Math.Max(result, leftCount[i] + rightCount[k - i]);
+            }
+            return result;
+        }
+
+        //3,4,2,3
+        //1,2,3,1,2,3
+        //1,2,3,1,4,5
+        public bool CheckPossibility(int[] nums)
+        {
+
+            bool IsChange = false;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (IsChange && nums[i] < nums[i - 1])
+                {
+                    return false;
+                }
+                if (nums[i] < nums[i - 1])
+                {
+                    IsChange = true;
+                    if (i > 1 && nums[i] < nums[i - 2])
+                    {
+                        nums[i] = nums[i - 1];
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/978/978Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._978
+{
+    class _978Solution
+    {
+        //当 A 的子数组 A[i], A[i + 1], ..., A[j] 满足下列条件时，我们称其为湍流子数组：
+
+        //若 i <= k<j，当 k 为奇数时， A[k]> A[k + 1]，且当 k 为偶数时，A[k] < A[k + 1]；
+        //或 若 i <= k<j，当 k 为偶数时，A[k]> A[k + 1] ，且当 k 为奇数时， A[k] < A[k + 1]。
+        //也就是说，如果比较符号在子数组中的每个相邻元素对之间翻转，则该子数组是湍流子数组。
+
+        //返回 A 的最大湍流子数组的长度。
+        /// [9,4,2,10,7,8,8,1,9]
+        /// 5
+        /// A[1] > A[2] < A[3] > A[4] < A[5]
+        ///  1<= A.length <= 40000
+        ///  [0,8,45,88,48,68,28,55,17,24]
+        public int MaxTurbulenceSize(int[] arr)
+        {
+            int result = 1;
+            int left = 0;
+            int type = 3;//1大 2 小 3 同
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if ((type == 1 && arr[i] >= arr[i - 1]) ||
+                    (type == 2 && arr[i] <= arr[i - 1]) ||
+                    arr[i] == arr[i - 1])
+                {
+                    result = Math.Max(result, i - left);
+                    left = arr[i] == arr[i - 1] ? i : i - 1;
+                }
+                type = arr[i] == arr[i - 1] ? 3 : arr[i] > arr[i - 1] ? 1 : 2;
+            }
+            result = Math.Max(result, arr.Length - left);
+            return result;
+        }
+    }
+}
